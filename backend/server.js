55f83ae1aa3f090d56aaddf3b07839501f8769e6
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-11-08 19:21:56 +0600

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv, { config } from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
+import connectDB from './config/db.js';
+
+connectDB();
 
 const app = express();
 
 app.listen(port, () => {
     console.log(`Server runs on port : ${port}`);
 });
+
+//AMv8XDBcLR4acVav
 
