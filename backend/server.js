74725ba48a630c5beeb62ed4f36e3bdc731be970
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-11-08 21:07:53 +0600

    create test api route

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
 import connectDB from './config/db.js';
+import userRouter from './routes/userRoutes.js';
 
 connectDB();
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
     console.log(`Server runs on port : ${port}`);
