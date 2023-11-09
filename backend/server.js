f7c12654ecd9fdf85d4c2d654b200a1caf15043a
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-11-09 19:15:48 +0600

    create sign up api route

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors';
 import colors from 'colors';
 import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
 import connectDB from './config/db.js';
 import userRouter from './routes/userRoutes.js';
+import authRouter from './routes/authRoutes.js';
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
     console.log(`Server runs on port : ${port}`);
