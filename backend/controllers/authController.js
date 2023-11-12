5866526bb364b084ffc39e7767bb6c67570ca068
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-11-12 17:01:15 +0600

    create sign in api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
-// import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -15,9 +16,27 @@ const signUp = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-    
+};
+
+const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(401, "User Not Found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(404, "Wrong Credentials"));
+        const token = jwt.sign({id: validUser._id}, process.env.SECRET_JWT);
+        const {password: pass, ...rest} = validUser._doc;
+        res
+            .cookie('access_token', token, {httpOnly: true})
+            .status(200)
+            .json(rest)
+    } catch (error) {
+        next(error);
+    }
 };
 
 export {
     signUp,
+    signIn,
 }
