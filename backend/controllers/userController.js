b0ffb74d00c9dffe4b41d6ebd02c7196d7d3872f
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-12-19 11:15:12 +0600

    create get user listings api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listingModel.js";
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
@@ -47,11 +48,24 @@ const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-    
+};
+
+const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        return next(errorHandler(401, "You can only view your own listings!"));
+    }
 };
 
 export {
     test,
     updateUser,
     deleteUser,
+    getUserListings,
 }
