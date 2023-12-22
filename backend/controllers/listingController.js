37b7235d913d05156609d88c2e11747194721985
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-12-22 22:00:46 +0600

    complete delete user listing functionality

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 const createListing = async (req, res, next) => {
     try {
@@ -9,6 +10,28 @@ const createListing = async (req, res, next) => {
     }
 };
 
+const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        next(errorHandler(404, "Listing not found!"));
+        return;
+    }
+
+    if (req.user.id !== listing.userRef) {
+        next(errorHandler(401, "You can only delete your own listings!"));
+        return;
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("Listing has been deleted!");
+    } catch (error) {
+        next(error);
+    }
+};
+
 export {
     createListing,
+    deleteListing,
 }
